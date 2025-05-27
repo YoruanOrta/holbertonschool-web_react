ac7a769e2d8bdec20c295275dc186533f55d10d5
YoruanOrta <yoruanorta50@gmail.com>
2025-05-27 14:15:34 -0400

    Enhance TypeScript interfaces by adding Directors interface and updating example usage

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,20 +1,36 @@
 // Define the Teacher interface
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
-  }
-  
-  // Example usage
-  const teacher3: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: false,
-    location: 'London',
-    contract: false,
-  };
-  
-  console.log(teacher3);
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+// Example usage of Teacher interface
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+// Extend the Teacher interface to create Directors
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage of Directors interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
