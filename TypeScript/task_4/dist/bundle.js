65be9203c229d48ad41d90957c57e82846fc27c9
YoruanOrta <yoruanorta50@gmail.com>
2025-05-30 11:27:47 -0400

    Add TypeScript task 4 files including subjects, HTML template, and Webpack configuration

---
+++ TypeScript/task_4/dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+// main.ts (stub to satisfy webpack)
+console.log("Build complete.");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLG9DQUFvQztBQUNwQyxPQUFPLENBQUMsR0FBRyxDQUFDLGlCQUFpQixDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90eXBlc2NyaXB0X2RlcGVuZGVuY2llcy8uL2pzL21haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gbWFpbi50cyAoc3R1YiB0byBzYXRpc2Z5IHdlYnBhY2spXG5jb25zb2xlLmxvZyhcIkJ1aWxkIGNvbXBsZXRlLlwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
